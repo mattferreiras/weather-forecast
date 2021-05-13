4539f1d939441a3a42dff30d54c36a3df89e8d3a
mattferreiras <mattferreiras@gmail.com>
2021-05-12 23:39:07 -0300

    start class and application properties

---
+++ src/main/kotlin/br/com/weather/forecast/ml/WeatherForecastMLApplication.kt
@@ -0,0 +1,11 @@
+package br.com.weather.forecast.ml
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WeatherForecastMLApplication
+
+fun main() {
+    SpringApplication.run(WeatherForecastMLApplication::class.java)
+}
